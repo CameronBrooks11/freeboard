9f44eb0d9ca963302de2341fc7bbbe239b20fc33
Cam Brooks <cambrooks3393@gmail.com>
2025-08-03 14:19:50 -0400

    update player

--- ansible/player.sh
+++ ansible/player.sh
@@ -2,27 +2,32 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-until curl --output /dev/null --silent --head --fail $FREEBOARD_PLAYER_URL; do
+until curl --output /dev/null --silent --head --fail "$SUPERBOARD_PLAYER_URL"; do
     printf '.'
     sleep 5
 done
 
-flags=(
-   --kiosk
-   --window-size=1920,1080
-   --window-position=0,0
-   --touch-events=enabled
-   --disable-pinch
-   --noerrdialogs
-   --disable-session-crashed-bubble
-   --simulate-outdated-no-au='Tue, 31 Dec 2099 23:59:59 GMT'
-   --disable-component-update
-   --overscroll-history-navigation=0
-   --disable-features=TranslateUI
-   --autoplay-policy=no-user-gesture-required
-   --use-fake-ui-for-media-stream
-)
-
 export DISPLAY=:0.0
 
-sudo -u pi startx /usr/bin/chromium-browser "${flags[@]}" --app=$FREEBOARD_PLAYER_URL -- -nocursor
+RES=$(fbset | grep geometry | awk '{print $2"x"$3}')
+WIDTH=$(echo "$RES" | cut -d'x' -f1)
+HEIGHT=$(echo "$RES" | cut -d'x' -f2)
+
+flags=(
+    --kiosk
+    --start-fullscreen
+    --window-position=0,0
+    --touch-events=enabled
+    --disable-pinch
+    --noerrdialogs
+    --disable-session-crashed-bubble
+    --simulate-outdated-no-au='Tue, 31 Dec 2099 23:59:59 GMT'
+    --disable-component-update
+    --overscroll-history-navigation=0
+    --disable-features=TranslateUI
+    --autoplay-policy=no-user-gesture-required
+    --use-fake-ui-for-media-stream
+    --no-sandbox
+)
+
+sudo --preserve-env=SUPERBOARD_USER -u "${SUPERBOARD_USER}" startx /usr/bin/chromium-browser "${flags[@]}" --app="$SUPERBOARD_PLAYER_URL" # -- -nocursor
